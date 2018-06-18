74231c1173dcdacbc8d58b37bb4032d7ffc44cca
kentangSJTU <kentang@sjtu.edu.cn>
2018-06-18 11:24:56 +0800

    Add oauth authorization.

--- const.dart
+++ const.dart
@@ -1,4 +1,4 @@
 class constants {
-    static String APP_ID = "863rEoGllxma33KvSnjP";
-    static String APP_SECRET = "1FBFC8AE7091FB38082F7C7A0DA83743FD103875791A4989";
+    static String APP_ID = "";
+    static String APP_SECRET = "";
 }
